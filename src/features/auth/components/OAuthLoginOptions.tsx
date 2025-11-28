c2c1d2d4562c415ebb0892eee70f71cb2f8ce5d7
Ayush Ranjan Sinha <asayushranjansinha@gmail.com>
2025-11-28 14:50:59 +0530

    feat: add GitHub and Google OAuth social login (#37)

--- src/features/auth/components/OAuthLoginOptions.tsx
+++ src/features/auth/components/OAuthLoginOptions.tsx
@@ -1,15 +1,38 @@
+"use client";
+
 import Image from "next/image";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+
+import { cn } from "@/lib/utils";
+import { authClient } from "@/lib/auth-client";
 
 import { Button } from "@/components/ui/button";
-import { cn } from "@/lib/utils";
 
 type Props = {
   className?: string;
 };
-export const OAuthLoginOptions = ({ className }: Props) => {
+
+export const OAuthLoginOptions: React.FC<Props> = ({ className }) => {
+  const router = useRouter();
+  const { signIn } = authClient;
+
+  const handleOAuthLogin = async (provider: "github" | "google") => {
+    const toastId = toast.loading("Authenticating...");
+    const { error, data } = await signIn.social({ provider });
+
+    if (error) {
+      toast.error("OAuth failed. Try again.", { id: toastId });
+      return;
+    }
+
+    toast.success("You're in.", { id: toastId });
+    router.replace("/");
+  };
+
   return (
     <div className={cn("flex flex-col gap-6", className)}>
-      <Button variant={"outline"}>
+      <Button variant={"outline"} onClick={() => handleOAuthLogin("google")}>
         <Image
           src={"/google.svg"}
           alt="Login with Google"
@@ -19,7 +42,7 @@ export const OAuthLoginOptions = ({ className }: Props) => {
         />
         Continue with Google
       </Button>
-      <Button variant={"outline"}>
+      <Button variant={"outline"} onClick={() => handleOAuthLogin("github")}>
         <Image
           src={"/github.svg"}
           alt="Login with Github"
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,4 @@
-import {
-  checkout,
-  polar,
-  portal,
-  usage,
-  webhooks,
-} from "@polar-sh/better-auth";
+import { checkout, polar, portal } from "@polar-sh/better-auth";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 
@@ -18,6 +12,16 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
     autoSignIn: true,
+  },
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
   },
   plugins: [
     polar({

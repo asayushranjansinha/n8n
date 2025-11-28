b101ae9dd0aed464c908a9f4a8154822db8b0597
Ayush Ranjan Sinha <asayushranjansinha@gmail.com>
2025-11-28 17:56:41 +0530

    fix:test prisma engine issue by manually copying client location

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,10 @@ const nextConfig: NextConfig = {
   async redirects() {
     return [{ source: "/", destination: "/workflows", permanent: false }];
   },
+  outputFileTracingIncludes: {
+    '/api/**/*': ['./node_modules/.prisma/client/**/*'],
+    '/*': ['./node_modules/.prisma/client/**/*'],
+  },
 };
 
 export default withSentryConfig(nextConfig, {
